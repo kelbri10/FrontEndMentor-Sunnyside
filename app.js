90f1d6dcccaff090cb2267d39ce2d4b9133ec0fd
Kelsey Hughes <kelbri10@gmail.com>
2023-04-27 03:17:11 -0400

    added hamburger as button, toggle on click

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('.menu-toggle'); 
+
+const menuToggle = () =>{ 
+    const navLinks = document.querySelector('.nav-links'); 
+
+    navLinks.classList.toggle('hidden'); 
+}
