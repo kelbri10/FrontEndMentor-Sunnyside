4a73d2264119c126b37055870f9259666533d440
Kelsey Hughes <kelbri10@gmail.com>
2023-05-02 14:33:12 -0400

    refactored css and html

--- app.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector('.menu-toggle'); 
-
-const menuToggle = () =>{ 
-    const navLinks = document.querySelector('.nav-links'); 
-
-    navLinks.classList.toggle('hidden'); 
-}
