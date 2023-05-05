92e15452e05280ddb9cef72d844dc9ee09a0871f
Kelsey Hughes <kelbri10@gmail.com>
2023-05-04 23:27:34 -0400

    reworked hamburger menu, added desktop and mobile nav seperate components

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const navMenu = document.querySelector('.nav__menu'); 
-const menuToggle = document.querySelector('.menu__toggle'); 
+const toggleBtn = document.querySelector('.menu__toggle'); 
+const navMenu = document.querySelector('.mobileNav__menu'); 
 
-menuToggle.addEventListener('click', () => { 
+const toggleMenu = () =>{ 
     navMenu.classList.toggle('active'); 
-})
+}
+
+toggleBtn.addEventListener('click', toggleMenu); 
